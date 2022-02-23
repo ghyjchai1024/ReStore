b2b15470fcb88e6967b7294e9c30665613008733
ghyjchai1024 <ron.yongjie@gmail.com>
2022-02-23 15:21:01 +0800

    End of section 2

---
+++ API/Data/Migrations/20220223065557_ProductTableUpdated.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class ProductTableUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Picture",
+                table: "Products",
+                newName: "Type");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PictureUrl",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PublicId",
+                table: "Products",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PictureUrl",
+                table: "Products");
+
+            migrationBuilder.DropColumn(
+                name: "PublicId",
+                table: "Products");
+
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Products",
+                newName: "Picture");
+        }
+    }
+}
